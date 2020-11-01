97112fcc4b26aac3a979d0c754c0b0082705d73f
Max Ruth <mer335@cornell.edu>
2020-11-01 09:17:08 -0500

    oops

--- src/ldriver.c
+++ src/ldriver.c
@@ -272,12 +272,12 @@ int run_sim(lua_State* L)
             tcompute += elapsed;
             printf("  Time: %e (%e for %d steps)\n", elapsed, elapsed/nstep, nstep);
 		}
-*/		
+	
 //        tcompute += elapsed;
 //        printf("  Time: %e (%e for %d steps)\n", elapsed, elapsed/nstep, nstep);
     }
 //    printf("Total compute time: %e\n", tcompute);
-    
+*/    
 	if(sim->rank == 0){
         printf("Total compute time: %e\n", tcompute);
 		viz_close(viz); // We have only opened a file for the rank=0 node 
